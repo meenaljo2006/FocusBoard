613a80b369fb4821e1eb6c0c61db5edeb5fd7247
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-04-28 01:50:47 +0530

    Remove unwanted file

--- Java Language/Basics/variable.java
+++
@@ -1,9 +0,0 @@
-public class variable {
-    public static void main(String[] args) {
-        int num1 = 25;
-        int num2 = 2;
-        System.out.println(num1);
-        System.out.println(num2);
-
-    }
-}
--- Java Language/Bit Manipulation/Add_1.java
+++
@@ -1,9 +0,0 @@
-public class Add_1 {
-
-    public static void main(String[] args) {
-        
-        int num = 5;
-        System.out.println(-~num);    //  -num = ~num+1 replace ~num by num  --->   -~num = num+1
-    }
-    
-}
