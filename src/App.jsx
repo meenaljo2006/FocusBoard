7039734ef086275ab9b4e3553db74f1305a750c4
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-05-20 05:23:42 +0530

    finalCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./appRoutes";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <Router>
       <AppRoutes />
+      <SpeedInsights />
     </Router>
   );
 }
