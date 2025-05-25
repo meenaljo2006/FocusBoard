5ebd89c2223e9b3783903a0022fb6d00abf6b4e4
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-05-25 22:39:42 +0530

    finalCommit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/FocusBoard"
+  base: process.env.NODE_ENV === 'production' ? '/FocusBoard/' : '/',
 })
